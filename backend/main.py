366bbcd5a075251303dfb0ee0c850aebab6d5082
Phyeory <jamjamfr@proton.me>
2026-08-20 21:54:38 +0100

    chores: removed the status logs

--- backend/main.py
+++ backend/main.py
@@ -2150,5 +2150,5 @@ async def live_trading_ws(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="info", access_log=False)
 
